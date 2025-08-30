130fc4ab69cc5afec5061349ba47b8ba6d2d18d1
Srikanth Tangella <srikanth.tangella@gmail.com>
2025-08-30 18:26:21 -0500

    Created Common Views

---
+++ BasicControls/Common/ControlIntroductionView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace BasicControls.Common
+{
+    /// <summary>
+    /// Interaction logic for ControlIntroductionView.xaml
+    /// </summary>
+    public partial class ControlIntroductionView : UserControl
+    {
+        public ControlIntroductionView()
+        {
+            InitializeComponent();
+        }
+    }
+}
